fede5d5ad9188ade88d412c9fed04e8baf4c1bc8
dukeguy <dgladwin@gmail.com>
2026-07-27 18:50:48 +0100

    4. Remove ads for web version

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,23 @@
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.assetExts.push('db');
+// Intercept react-native-google-mobile-ads imports when bundling for web
+const originalResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-google-mobile-ads') {
+    return {
+      filePath: path.resolve(__dirname, 'mocks/react-native-google-mobile-ads.js'),
+      type: 'source_file',
+    };
+  }
+
+  if (originalResolveRequest) {
+    return originalResolveRequest(context, moduleName, platform);
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
