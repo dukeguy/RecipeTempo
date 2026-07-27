42c7650fc6ff68f8044d6954fd694aabe3631513
dukeguy <dgladwin@gmail.com>
2026-07-27 19:04:45 +0100

    7. Remove ads for web version

--- metro.config.js
+++ metro.config.js
@@ -3,21 +3,9 @@ const path = require('path');
 
 const config = getDefaultConfig(__dirname);
 
-const originalResolveRequest = config.resolver.resolveRequest;
-
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (platform === 'web' && moduleName === 'react-native-google-mobile-ads') {
-    // Return a direct file path string or a properly structured object
-    return {
-      type: 'source_file',
-      filePath: path.resolve(__dirname, 'mocks/react-native-google-mobile-ads.js'),
-    };
-  }
-
-  if (originalResolveRequest) {
-    return originalResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  'react-native-google-mobile-ads': path.resolve(__dirname, 'mocks/react-native-google-mobile-ads.js'),
 };
 
 module.exports = config;
